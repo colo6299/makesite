e727dabc8a81c6c270e65fdae8ec0a4db0835ff3
= <=>
2020-06-05 17:20:42 -0700

    Add username/repo for report card

--- makesite.go
+++ makesite.go
@@ -1,12 +1,12 @@
 package main
 
-<<<<<<< HEAD
 import (
+	"fmt"
 	"io/ioutil"
 )
 
 func main() {
-
+	fmt.Printf("%d\n", readFile())
 }
 
 func readFile() string {
@@ -18,11 +18,6 @@ func readFile() string {
 	return string(fileContents)
 }
 
-func renderTemplate()
-=======
-import "fmt"
+func renderTemplate() {
 
-func main() {
-	fmt.Println("Hello, world!")
 }
->>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
