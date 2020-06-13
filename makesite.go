9f3863297fc17297299663056c36913c62837d6b
colo6299 <rocketblox@gmail.com>
2020-06-13 09:27:48 +0900

    Add recursive file generation and console logging

--- makesite.go
+++ makesite.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
 type pageData struct {
@@ -15,35 +18,68 @@ type pageData struct {
 
 func main() {
 
+	startTime := time.Now()
+
 	fileFlag := flag.String("file", "first-post.txt", "used to define input text")
 	dirFlag := flag.String("dir", "none", "if spedified, generates for all .txt in the directory")
+	outDirFlag := flag.String("out", "output_html/", "SSG output directory")
 	flag.Parse()
+
+	var fileCount int = 0
+	var fileSize float64 = 0
+
 	if *dirFlag == "none" {
-		runForFile(*fileFlag, "text_directory/")
+		runForFile(*fileFlag, "text_directory/", &fileCount, &fileSize)
 	} else {
 		var directory string = *dirFlag
-		if directory[len(directory)-1] != "/"[0] {
-			directory += "/"
-		}
+		runForDir(directory, *outDirFlag, &fileCount, &fileSize)
+	}
 
-		files, err := ioutil.ReadDir(directory)
-		if err != nil {
-			panic(err)
-		}
-		for _, file := range files {
-			if file.IsDir() == false {
-				runForFile(file.Name(), directory)
-			}
+	elapsedTimeFloat := time.Since(startTime).Seconds()
+	elapsedTime := strconv.FormatFloat(elapsedTimeFloat, 'f', 6, 64)
+	elapsedTime = elapsedTime[0 : strings.Index(elapsedTime, ".")+3]
+
+	sizeString := strconv.FormatFloat(fileSize, 'f', -1, 32)
+	sizeString = sizeString[0 : strings.Index(sizeString, ".")+3]
+
+	fmt.Printf(
+		"\n\033[32;1mSuccess!\033[0m Created \033[1m%d\033[0m files with a size of %skb in %s seconds!\n",
+		fileCount,
+		sizeString,
+		elapsedTime,
+	)
+
+}
+
+func runForDir(directory, out string, fileCountPtr *int, fileSizePtr *float64) {
+	if directory[len(directory)-1] != "/"[0] {
+		directory += "/"
+	}
+
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() == false {
+			runForFile(file.Name(), directory, fileCountPtr, fileSizePtr)
+		} else {
+			runForDir(directory+"/"+file.Name(), out, fileCountPtr, fileSizePtr)
 		}
 	}
 }
 
-func runForFile(fileFlag, directory string) {
+func runForFile(fileFlag, directory string, fileCountPtr *int, fileSizePtr *float64) {
 	var fileName string = fileFlag
+	if fileName[strings.Index(fileFlag, "."):len(fileFlag)] != ".txt" {
+		return
+	}
+
 	fileName = fileName[0:strings.Index(fileFlag, ".")] + ".html"
 
 	var textData string = readFile(directory + fileFlag)
-	renderTemplate("template.tmpl", textData, fileName)
+	renderTemplate("template.tmpl", textData, fileName, fileCountPtr, fileSizePtr)
 }
 
 func readFile(inPath string) string {
@@ -55,12 +91,12 @@ func readFile(inPath string) string {
 	return string(fileContents)
 }
 
-func renderTemplate(tPath, textData, fileName string) {
+func renderTemplate(tPath, textData, fileName string, fileCountPtr *int, fileSizePtr *float64) {
 	paths := []string{
 		tPath,
 	}
 
-	f, err := os.Create(fileName)
+	f, err := os.Create("output_html/" + fileName)
 	if err != nil {
 		panic(err)
 	}
@@ -76,6 +112,13 @@ func renderTemplate(tPath, textData, fileName string) {
 	if err != nil {
 		panic(err)
 	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+	*fileCountPtr++
+	*fileSizePtr += float64(fi.Size()) / float64(1000) //1000 or 1024? I think 1024 is kibabite but honestly I don't really care
 
 	f.Close()
 
